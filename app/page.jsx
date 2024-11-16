1cc9b7fb0ee067da5e6219826646f91eedc6f12b
DikyGIT <dikyoga27@gmail.com>
2024-11-16 14:09:55 +0700

    Topic List UI

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import TopicList from "@/components/TopicList";
 
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <TopicList />
+    </>
+  );
 }
